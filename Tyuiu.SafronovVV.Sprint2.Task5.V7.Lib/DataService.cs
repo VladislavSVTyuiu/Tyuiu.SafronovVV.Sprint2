803180b3914d28f3a5e7ce427facabc435813359
X-Ray2003 <134064048+X-Ray2003@users.noreply.github.com>
2023-10-10 20:58:59 +0500

    Task5

---
+++ Tyuiu.SafronovVV.Sprint2.Task5.V7.Lib/DataService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.SafronovVV.Sprint2.Task5.V7.Lib
+{
+    public class DataService : ISprint2Task5V7
+    {
+        public string FindMonthName(int startYear, int n)
+        {
+            {
+                while (n >= 12)
+                {
+                    n -= 12;
+                }
+
+                string res;
+                switch (n)
+                {
+                    case 0:
+                        res = "Январь";
+                        break;
+                    case 1:
+                        res = "Февраль";
+                        break;
+                    case 2:
+                        res = "Март";
+                        break;
+                    case 3:
+                        res = "Апрель";
+                        break;
+                    case 4:
+                        res = "Май";
+                        break;
+                    case 5:
+                        res = "Июнь";
+                        break;
+                    case 6:
+                        res = "Июль";
+                        break;
+                    case 7:
+                        res = "Август";
+                        break;
+                    case 8:
+                        res = "Сентябрь";
+                        break;
+                    case 9:
+                        res = "Октябрь";
+                        break;
+                    case 10:
+                        res = "Ноябрь";
+                        break;
+                    case 11:
+                        res = "Декабрь";
+                        break;
+
+                    default:
+                        throw new ArgumentException($"Месяц должен быть от 1 до 12. Введенное значение {n}");
+                }
+                return res;
+            }
+        }
+    }
+}
---
+++ Tyuiu.SafronovVV.Sprint2.Task5.V7/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SafronovVV.Sprint2.Task5.V7.Lib;
+
+namespace Tyuiu.SafronovVV.Sprint2.Task5.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Сафронов В. В. | АСОиУб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант 7                                                               *");
+            Console.WriteLine("* Выполнил: Сафронов Владислав Валерьевич | АСОиУб-23-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch, вычисляет       *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("* Условие: С начала 1990 года по некоторый день прошло n месяцев и 2 дня. *");
+            Console.WriteLine("* Определить название месяца (январь, февраль и т. п.) этого дня.         *");
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Укажите с какого года ведется отсчёт: ");
+            int startYear = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Укажите сколько месяцев прошло со 2 января этого года: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ: *");
+            Console.WriteLine("***************************************************************************");
+
+            string resultMonthName = ds.FindMonthName(startYear, n);
+            Console.WriteLine($"С начала {startYear} года прошло " + n + " месяцев");
+            Console.WriteLine("Тогда сейчас " + resultMonthName);
+            Console.ReadKey();
+        }
+    }
+}
